b99f6ab94ca0032063c467b2c3bb89558cfe27d7
iwnuplynottyan <wolflast21@gmail.com>
2025-08-26 08:25:18 +0300

    upd

--- host/lira/configuration.nix
+++ host/lira/configuration.nix
@@ -1,8 +1,9 @@
+
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ inputs, outputs, pkgs, ... }:
 
 {
   imports =
@@ -10,84 +11,71 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+  # Boot
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  hardware.enableRedistributableFirmware = true;
-
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Kyiv";
-
-  # Select internationalisation properties.
+  # Hostname
+  networking.hostName = "nixos";
+  
+  # Locale and timezone
   i18n.defaultLocale = "en_US.UTF-8";
-
+  time.timeZone = "Europe/Kyiv";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "uk_UA.UTF-8";
-    LC_IDENTIFICATION = "uk_UA.UTF-8";
-    LC_MEASUREMENT = "uk_UA.UTF-8";
-    LC_MONETARY = "uk_UA.UTF-8";
-    LC_NAME = "uk_UA.UTF-8";
-    LC_NUMERIC = "uk_UA.UTF-8";
-    LC_PAPER = "uk_UA.UTF-8";
-    LC_TELEPHONE = "uk_UA.UTF-8";
-    LC_TIME = "uk_UA.UTF-8";
+  LC_ADDRESS = "uk_UA.UTF-8";
+  LC_IDENTIFICATION = "uk_UA.UTF-8";
+  LC_MEASUREMENT = "uk_UA.UTF-8";
+  LC_MONETARY = "uk_UA.UTF-8";
+  LC_NAME = "uk_UA.UTF-8";
+  LC_NUMERIC = "uk_UA.UTF-8";
+  LC_PAPER = "uk_UA.UTF-8";
+  LC_TELEPHONE = "uk_UA.UTF-8";
+  LC_TIME = "uk_UA.UTF-8";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Kernel
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  
+  # User
   users.users.q = {
     isNormalUser = true;
-    description = "lira";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    description = "iwnuplylo";
+    extraGroups = [ "networkmanager" "wheel" "audio" ];
+    packages = with pkgs; [
+      firefox
+     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  # Network
+  networking.networkmanager.enable = true;
+  # OpenSSH
   services.openssh.enable = true;
+  
+  # PKGS
+  environment.systemPackages = {
+  	shells = with pkgs; [ zsh ];
+	systemPackages 	= with pkgs; [
+  
+  neovim
+  git
+  tmux
+  doas
+  ];
+  };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Doas Enable & Config
+  security = {
+    doas = {
+    enable = true;
+  extraConfig = ''
+  permit persist keepenv :wheel
+  '';
+  };
+  };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  # Sudo Disable
+  security.sudo.enable = false; # Sudo Disable
 
+  # System
+  system.stateVersion = "23.05";
 }
