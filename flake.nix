c35ee0a3cc2afd27951f39fc50ab9d1b2ef8db31
iwnuplynottyan <wolflast21@gmail.com>
2025-08-26 07:54:01 +0300

    upd

--- flake.nix
+++ flake.nix
@@ -60,11 +60,11 @@
 	inherit system;
 	modules = [
 	  ./host/lira/configuration.nix
-	  home-manager.nixosModules.home-manager {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.q = import ./home.nix;
-	  }
+	 # home-manager.nixosModules.home-manager {
+	 #   home-manager.useGlobalPkgs = true;
+	 #   home-manager.useUserPackages = true;
+	 #   home-manager.users.q = import ./home.nix;
+	 # }
 	];
       };
     };
