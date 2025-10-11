d1a09e8ed3f36bf270cddc3811d47adcd661adff
IwnuplyNotTyan <wolflast21@gmail.com>
2025-10-11 19:54:01 +0300

    clean up

--- host/eweless3/configuration.nix
+++ host/eweless3/configuration.nix
@@ -12,36 +12,36 @@
     ];
 
   # Virt
-  users.groups.libvirtd.members = ["q"];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
+  #users.groups.libvirtd.members = ["q"];
+  #virtualisation.libvirtd.enable = true;
+  #virtualisation.spiceUSBRedirection.enable = true;
 
   # DNS
-    services.stubby = {
-      enable = true;
-      settings = pkgs.stubby.passthru.settingsExample // {
-        upstream_recursive_servers = [{
-          address_data = "1.1.1.1";
-          tls_auth_name = "cloudflare-dns.com";
-          tls_pubkey_pinset = [{
-            digest = "sha256";
-            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
-          }];
-        } {
-          address_data = "1.0.0.1";
-          tls_auth_name = "cloudflare-dns.com";
-          tls_pubkey_pinset = [{
-            digest = "sha256";
-            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
-          }];
-        }];
-      };
-    };
+  #  services.stubby = {
+  #    enable = true;
+  #    settings = pkgs.stubby.passthru.settingsExample // {
+  #      upstream_recursive_servers = [{
+  #        address_data = "1.1.1.1";
+  #        tls_auth_name = "cloudflare-dns.com";
+  #        tls_pubkey_pinset = [{
+  #          digest = "sha256";
+  #          value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+  #        }];
+  #      } {
+  #        address_data = "1.0.0.1";
+  #        tls_auth_name = "cloudflare-dns.com";
+  #        tls_pubkey_pinset = [{
+  #          digest = "sha256";
+  #          value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+  #        }];
+  #      }];
+  #    };
+  #  };
 
   # Distrobox
-  virtualisation.podman = {
-  enable = true;
-  dockerCompat = true;
+  #virtualisation.podman = {
+  #enable = true;
+  #dockerCompat = true;
   };
 
   # Boot
@@ -49,21 +49,21 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Hostname
-  networking.hostName = "nixos";
+  networking.hostName = "eweless3";
   
   # Disk
   services.udisks2.enable = true;
   services.udisks2.mountOnMedia = true;
 
   # OpenCL
-   hardware.graphics = { # hardware.opengl in 24.05
-    enable = true;
-    enable32Bit = true; # driSupport32Bit in 24.05
-    extraPackages = with pkgs; [
-      intel-compute-runtime
-      rocmPackages.clr.icd
-    ];
-  };
+  # hardware.graphics = { # hardware.opengl in 24.05
+  #  enable = true;
+  #  enable32Bit = true; # driSupport32Bit in 24.05
+  #  extraPackages = with pkgs; [
+  #    intel-compute-runtime
+  #    rocmPackages.clr.icd
+  #  ];
+  #};
 
   # Locale and timezone
   i18n.defaultLocale = "en_US.UTF-8";
@@ -89,7 +89,7 @@
     description = "iwnuplylo";
     extraGroups = [ "networkmanager" "wheel" "audio" ];
     packages = with pkgs; [
-      firefox
+      #firefox
      ];
   };
 
@@ -108,7 +108,7 @@
   alsa.enable = true;
   alsa.support32Bit = true;
   pulse.enable = true;
-  jack.enable = true;
+  #jack.enable = true;
   #media-session.enable = true;
   };
 
@@ -156,12 +156,12 @@
 	systemPackages 	= with pkgs; [
   
   # Nix
-  nvd
-  nh
-  nix-output-monitor
+  #nvd
+  #nh
+  #nix-output-monitor
 
   # CLI & TUI
-  distrobox
+  #distrobox
   doas
   neovim
   git
@@ -169,7 +169,7 @@
   joshuto
   btop
   eza
-  amfora
+  #amfora
   zsh
   starship
   tmux
@@ -184,19 +184,21 @@
   maim
   
   # Games
-   #ddnet
-  vitetris
+  #ddnet
+  #vitetris
   steam
-  lutris
+  #lutris
   prismlauncher
-  jdk21
-  jdk17
-  jdk8
-  wineWow64Packages.minimal
+  #jdk21
+  #jdk17
+  #jdk8
+  #wineWow64Packages.minimal
 
-  # Arts
+  # Work's
   aseprite
-   #krita
+  krita
+  kdenlive
+  audacity
 
   # Xorg
   i3
@@ -205,7 +207,7 @@
   xclip
   xsel
   rofi
-  rofi-power-menu
+  #rofi-power-menu
   picom
   warpd
 
@@ -217,12 +219,12 @@
   xorg.xf86videointel # Intel
   
   # Sound
-  alsa-oss
+  #alsa-oss
   pavucontrol
-  libjack2
-  jack2
-  qjackctl
-  jack_capture
+  #libjack2
+  #jack2
+  #qjackctl
+  #jack_capture
   ];
   };
 
@@ -241,9 +243,9 @@
   };
 
   # ADB
-  services.udev.packages = [
-    pkgs.android-udev-rules
-  ];
+  #services.udev.packages = [
+  #  pkgs.android-udev-rules
+  #];
 
 
   # Sudo Disable
@@ -257,40 +259,40 @@
   };
 
   # Nix
-  nix = {
-    settings = {
-      trusted-users = [ 
-        "username"
-        "root"
-      ];
-      trusted-substituters = [
-        "https://nyx.chaotic.cx"
-      ];
-      extra-trusted-substituters = [
-        "https://nyx.chaotic.cx"
-      ];
-      substituters = [ 
-        "https://hyprland.cachix.org"
-        "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
-        "https://nyx.chaotic.cx"
-        "https://nix-gaming.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      ];
-      extra-trusted-public-keys = [
-        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-      ];
-      auto-optimise-store = true;
-      experimental-features = [ "nix-command" "flakes" ];
-    };
-  };
+  #nix = {
+  #  settings = {
+  #    trusted-users = [ 
+  #      "username"
+  #      "root"
+  #    ];
+  #    trusted-substituters = [
+  #      "https://nyx.chaotic.cx"
+  #    ];
+  #    extra-trusted-substituters = [
+  #      "https://nyx.chaotic.cx"
+  #    ];
+  #    substituters = [ 
+  #      "https://hyprland.cachix.org"
+  #      "https://cache.nixos.org/"
+  #      "https://nix-community.cachix.org"
+  #      "https://nyx.chaotic.cx"
+  #      "https://nix-gaming.cachix.org"
+  #    ];
+  #    trusted-public-keys = [
+  #      "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+  #      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+  #      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+  #      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  #      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+  #    ];
+  #    extra-trusted-public-keys = [
+  #      "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+  #      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+  #    ];
+  #    auto-optimise-store = true;
+  #    experimental-features = [ "nix-command" "flakes" ];
+  #  };
+  #};
 
   # Nixpkgs Rules
   nixpkgs = {
@@ -300,11 +302,11 @@
         "openssl-1.1.1u"
         "openssl-1.1.1w"
       ];
-      overlays = [
-        outputs.overlays.default
-        inputs.chaotic.overlays.default
-        inputs.nurpkgs.overlay
-      ];
+  #    overlays = [
+  #      outputs.overlays.default
+  #      inputs.chaotic.overlays.default
+  #      inputs.nurpkgs.overlay
+  #    ];
     };
   };
 
