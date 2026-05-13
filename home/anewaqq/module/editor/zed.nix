0a1300aa1984807bacf8294cc5ba450d05e58d93
IwnuplyNotTyan <wolflast21@gmail.com>
2026-05-13 22:47:42 +0300

    remove: zed

--- home/anewaqq/module/editor/zed.nix
+++ home/anewaqq/module/editor/zed.nix
@@ -1,6 +1,6 @@
 {
- lib,
- isDarwin,
+ #lib,
+ #isDarwin,
  ...
 }:
 
@@ -8,18 +8,18 @@
   programs.zed-editor.enable = true;
   home.file.".config/zed/themes/janleigh.json".source = ./zed.json;
 
-  xdg.desktopEntries = lib.mkIf (!isDarwin) {
-    zeditor = {
-      name = "Zed";
-      genericName = "Text Editor";
-      comment = "Text Editor";
-      exec = "nixGLIntel zeditor %u";
-      icon = "zed";
-      categories = [
-        "TextEditor"
-        "Development"
-      ];
-      terminal = false;
-    };
-  };
+  #xdg.desktopEntries = lib.mkIf (!isDarwin) {
+  #  zeditor = {
+  #    name = "Zed";
+  #    genericName = "Text Editor";
+  #    comment = "Text Editor";
+  #    exec = "nixGLIntel zeditor %u";
+  #    icon = "zed";
+  #    categories = [
+  #      "TextEditor"
+  #      "Development"
+  #    ];
+  #    terminal = false;
+  #  };
+  #};
 }
