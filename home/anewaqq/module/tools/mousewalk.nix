e6868e489f7e35ab7add651089d8afa5ef64172f
IwnuplyNotTyan <wolflast21@gmail.com>
2026-05-20 18:24:49 +0300

    feat: mousewalk desktop file (for non nixos)

--- home/anewaqq/module/tools/mousewalk.nix
+++ home/anewaqq/module/tools/mousewalk.nix
@@ -1,5 +1,19 @@
-{ mousewalkPkg, ... }:
+{ mousewalkPkg, lib, isDarwin, ... }:
 
 {
   home.packages = [ mousewalkPkg ];
+
+  xdg.desktopEntries = lib.mkIf (!isDarwin) {
+    mousewalk = {
+      name = "mousewalk";
+      genericName = "Mousewalk";
+      comment = "Mouse cursor screensaver";
+      exec = "mousewalk %u";
+      icon = "mousewalk";
+      categories = [
+        "Utility"
+      ];
+      terminal = false;
+    };
+  };
 }
