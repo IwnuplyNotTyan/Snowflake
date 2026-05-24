13f9ce71e003fce9953ca597175fb164b375355d
IwnuplyNotTyan <wolflast21@gmail.com>
2026-05-24 17:29:42 +0300

    feat(syncthing): folders

--- home/anewaqq/module/tools/syncthing.nix
+++ home/anewaqq/module/tools/syncthing.nix
@@ -1,16 +1,28 @@
 { isDarwin, ... }:
-
 {
-services.syncthing = {
-  enable = true;
-  settings = {
-    gui = { 
-    	user = if isDarwin then "Anewaqq-mac" else "Eweless3";
-    };
-    devices = {
-      "Merlinx" = { id = "L7CUYJH-7I7TYV3-3BAD5I7-35OTRR2-G6ZBNRP-DOAET6G-Y5CATHI-EU4B5QB"; };
-      #"Eweless3" = { id = "3UV3CJG-4UMLMS4-N5MOFIC-A7QMIOP-PR4KFKZ-EPPNUG3-4245VNA-RH3S7AX"; };
+  services.syncthing = {
+    enable = true;
+    settings = {
+      gui = {
+        user = if isDarwin then "Anewaqq-mac" else "Eweless3";
+      };
+      devices = {
+        "Merlinx" = { id = "L7CUYJH-7I7TYV3-3BAD5I7-35OTRR2-G6ZBNRP-DOAET6G-Y5CATHI-EU4B5QB"; };
+      };
+      folders = {
+        "Camera" = {
+          path = if isDarwin then "~/Documents/Camera" else "~/files/Camera";
+          devices = [ "Merlinx" ];
+        };
+        "Music" = {
+          path = if isDarwin then "~/Documents/Music" else "~/files/media/Music";
+          devices = [ "Merlinx" ];
+        };
+        "Wallpapers" = {
+          path = if isDarwin then "~/Documents/Wal" else "~/files/media/wal";
+          devices = [ "Merlinx" ];
+        };
+      };
     };
   };
-};
 }
