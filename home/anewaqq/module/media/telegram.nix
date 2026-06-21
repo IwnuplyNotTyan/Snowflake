ad257f14ee46eefcb827a90e70888c06751b6c26
IwnuplyNotTyan <ikissiwnuply@gmail.com>
2026-06-21 21:06:36 +0300

    chore: remove telegram icon

--- home/anewaqq/module/media/telegram.nix
+++ home/anewaqq/module/media/telegram.nix
@@ -1,23 +1,23 @@
-{ pkgs, lib, isDarwin, ...}:
+{ pkgs, ...}:
 {
   home.packages = with pkgs; [
     telegram-desktop
   ];
 
-  xdg.desktopEntries = lib.mkIf (!isDarwin) {
-    telegram-desktop = {
-      name = "Telegram Desktop";
-      comment = "Official Telegram messaging app";
-      exec = "nixGLIntel ${pkgs.telegram-desktop}/bin/telegram-desktop %u";
-      icon = "telegram";
-      terminal = false;
-      categories = [ "Network" "InstantMessaging" ];
-      mimeType = [ "x-scheme-handler/tg" ];
-      #startupWMClass = "TelegramDesktop";
-      #startupNotify = true;
-      settings = {
-        X-GNOME-UsesNotifications = "true";
-      };
-    };
-  };
+  #xdg.desktopEntries = lib.mkIf (!isDarwin) {
+  #  telegram-desktop = {
+  #    name = "Telegram Desktop";
+  #    comment = "Official Telegram messaging app";
+  #    exec = "nixGLIntel ${pkgs.telegram-desktop}/bin/telegram-desktop %u";
+  #    icon = "telegram";
+  #    terminal = false;
+  #    categories = [ "Network" "InstantMessaging" ];
+  #    mimeType = [ "x-scheme-handler/tg" ];
+  #    #startupWMClass = "TelegramDesktop";
+  #    #startupNotify = true;
+  #    settings = {
+  #      X-GNOME-UsesNotifications = "true";
+  #    };
+  #  };
+  #};
 }
