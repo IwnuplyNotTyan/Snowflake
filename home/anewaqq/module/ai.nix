a1ce3e6db7306a87adf503be31c05709e981e02a
IwnuplyNotTyan <ikissiwnuply@gmail.com>
2026-07-11 22:55:37 +0300

    fix: opencode install via npm if isDarwin

--- home/anewaqq/module/ai.nix
+++ home/anewaqq/module/ai.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, isDarwin, ... }:
+{ pkgs, pkgsUnstable, lib, isDarwin, ... }:
 
 {
   home.packages = [
@@ -6,15 +6,15 @@
   ] ++ lib.optionals (isDarwin) [
     pkgs.nodejs
   #] ++ lib.optionals (!isDarwin) [
-  #  pkgsUnstable.opencode
+    pkgsUnstable.opencode
   ];
 
-  home.activation.installOpencode = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  home.activation.installOpencode = lib.mkIf isDarwin (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     export PATH="${pkgs.nodejs}/bin:$PATH"
     export npm_config_prefix="$HOME/.npm-global"
     mkdir -p "$HOME/.npm-global/bin"
     if ! command -v opencode &>/dev/null; then
       $DRY_RUN_CMD npm install -g opencode-ai
     fi
-  '';
+  '');
 }
